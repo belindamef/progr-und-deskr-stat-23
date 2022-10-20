54d305325d9e35c71e2179e7d68876c7d6edfdb0
Belinda Fleischmann <fl.belinda@googlemail.com>
2022-10-20 15:26:18 +0200

    Fix formatting typo, add uebungsskript

--- Uebungsskript.R
+++
@@ -1,5 +0,0 @@
-# Übungsskript für die 2. Einheit in der Veranstaltung C1 
-# Autor: Belinda Fleischmann
-
-print("Hallo Welt!")  # Textausgabe
-print("Hallo R!")
---
+++ Uebungsskript_1.R
@@ -0,0 +1,11 @@
+# Mein erstes R Skript
+# dieses Skript enthält ein paar Übungsbeispiele
+# Autor: Belinda Fleischmann
+
+# Beispiel 1) Textausgabe
+print("Hallo Welt!")          # Textausgabe
+print("Hallo R!")             # Begrüße R
+
+# Beispiel 2) Arithmetik
+3 + 9                         # Addition
+sqrt(9)                       # Quadratwurzel
