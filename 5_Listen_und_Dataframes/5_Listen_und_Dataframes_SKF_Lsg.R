6c3223698edd8ae5f8ee222d18da79b65e60b6b2
Belinda Fleischmann <fl.belinda@googlemail.com>
2022-11-15 18:30:01 +0100

    Update (4) und (5)

--- 5_Listen_und_Dataframes/5_Listen_und_Dataframes_SKF_Lsg.R
+++ 5_Listen_und_Dataframes/5_Listen_und_Dataframes_SKF_Lsg.R
@@ -24,7 +24,7 @@ q_and_a = c(fragen, antworten)
 
 # ------SKF 10------
 iq_data_df = data.frame(id      = seq(101,130),
-                     age     = sample(18:99, 30, replace=T),
-                     iq      = rnorm(30, mean=100, sd=15),
-                     dropout = rbinom(30, 1, 0.05))
+                        age     = sample(18:99, 30, replace=T),
+                        iq      = rnorm(30, mean=100, sd=15),
+                        dropout = rbinom(30, 1, 0.05))
 
