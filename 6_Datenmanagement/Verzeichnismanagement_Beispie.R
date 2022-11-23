41efba6077f148cba677dfb9be345ec02084ba48
Belinda Fleischmann <fl.belinda@googlemail.com>
2022-11-23 09:47:44 +0100

    Update

--- 6_Datenmanagement/Verzeichnismanagement_Beispie.R
+++ 6_Datenmanagement/Verzeichnismanagement_Beispie.R
@@ -3,8 +3,9 @@
 # Directory and fname Management
 work_dir = getwd()                               # Working directory
 data_dir = file.path(work_dir, "Data")           # Datenverzeichnispfad
-fname= "cushny.csv"                              # (base) filename
-fpath= file.path(data_dir, fname)                # filepath
+rawdata_fname = "cushny.csv"                     # (base) filename
+fpath = file.path(data_dir, fname)               # filepath
+outdata_fname = "descr_stats.txt"
 
 # Read Data
 Data_df = read.table(fpath)                      # Einlesen der Datei
